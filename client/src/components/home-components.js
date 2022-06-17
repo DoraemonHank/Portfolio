0ab703aa0f55095c10144860f8e2305c82f3f0e2
FIRST_NAME LAST_NAME <tsj10226@gmail.com>
2022-06-17 17:26:02 +0800

    Add custom css class

--- client/src/components/home-components.js
+++ client/src/components/home-components.js
@@ -87,22 +87,22 @@ const HomeComponents = () => {
           <div className="row ">    
                 
             <div className="col-lg-4">
-              <img src="https://i.imgur.com/XgdEeuu.png" className="w-50 h-50" alt="" />
-              <h2>網頁開發</h2>
+              <img src="https://i.imgur.com/XgdEeuu.png" className="w-40 h-40" alt="" />
+              <h2 className="fs-4">網頁開發</h2>
               <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
               <p><a className="btn btn-outline-primary" href="#">View details &raquo;</a></p>
             </div>
 
             <div className="col-lg-4">
-              <img src="https://i.imgur.com/t8s6d1R.png" className="w-50 h-50" alt="" />
-              <h2>深度學習</h2>
+              <img src="https://i.imgur.com/t8s6d1R.png" className="w-40 h-40" alt="" />
+              <h2 className="fs-4">深度學習</h2>
               <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
               <p><a className="btn btn-outline-primary" href="#">View details &raquo;</a></p>
             </div>
 
             <div className="col-lg-4">
-              <img src="https://i.imgur.com/NkddNlL.jpg" className="w-50 h-50" alt="" />
-              <h2>遊戲設計</h2>
+              <img src="https://i.imgur.com/NkddNlL.jpg" className="w-40 h-40" alt="" />
+              <h2 className="fs-4">遊戲設計</h2>
               <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
               <p><a className="btn btn-outline-primary" href="#">View details &raquo;</a></p>
             </div>
