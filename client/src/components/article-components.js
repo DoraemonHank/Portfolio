955fe02e9ba24d25d0182a7d4f0d2a62b8e6a4cc
FIRST_NAME LAST_NAME <tsj10226@gmail.com>
2022-09-03 00:21:13 +0800

    Add loss file

---
+++ client/src/components/article-components.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import { useHistory } from 'react-router'
+
+const ArticleComponents = ({ article }) => {
+  const history = useHistory()
+  useEffect(() => {
+    console.log(JSON.parse(localStorage.getItem('Type')))
+    if (JSON.stringify(article) === '{}') {
+      history.push('/' + JSON.parse(localStorage.getItem('Type')))
+    }
+  }, [article])
+  return (
+
+    <div>
+        <section className="web"></section>
+        <div className="container">
+          <h4 className="text-center">{article.title}</h4>
+          <div className="d-flex justify-content-center">
+            <div className="" dangerouslySetInnerHTML={{ __html: article.content }}></div>
+          </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default ArticleComponents
